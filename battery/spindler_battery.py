3226b8a40766a480a64252e2cf68dd03e53e7f58
QuadlcorE <62242772+QuadlcorE@users.noreply.github.com>
2023-08-08 09:46:33 +0100

    Refactored the codebase

---
+++ battery/spindler_battery.py
@@ -0,0 +1,16 @@
+from abc import ABC 
+
+from battery import battery
+
+class SpindlerBattery(battery, ABC):
+    def __init__(self, last_service_date, current_date):
+        self.last_service_date = last_service_date
+        self.current_date = current_date
+    
+    def needs_service():
+        date_for_battery_service = add_years_to_date(self.last_service_date, 4)
+        if date_for_battery_service < self.current_date:
+            return True 
+        else: 
+            return False
+        
